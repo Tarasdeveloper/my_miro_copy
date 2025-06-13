e9a8767940b1056919117c46111f665548f083c3
Tarasdeveloper <tomahavki1@gmail.com>
2025-06-14 00:06:32 +0300

    added buttons

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,12 +1,77 @@
+import { rqClient } from "@/shared/api/instance";
 import { CONFIG } from "@/shared/model/config";
 import { ROUTES } from "@/shared/model/routes";
+import { Button } from "@/shared/ui/kit/button";
+import { Card, CardFooter, CardHeader } from "@/shared/ui/kit/card";
+import { useQueryClient } from "@tanstack/react-query";
 import { href, Link } from "react-router-dom";
 
 function BoardsListPage() {
+  const queryClient = useQueryClient();
+  const boardsQuery = rqClient.useQuery("get", "/boards");
+
+  const createBoardMutation = rqClient.useMutation("post", "/boards", {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(
+        rqClient.queryOptions("get", "/boards"),
+      );
+    },
+  });
+  const deleteBoardMutation = rqClient.useMutation(
+    "delete",
+    "/boards/{boardId}",
+    {
+      onSettled: async () => {
+        await queryClient.invalidateQueries(
+          rqClient.queryOptions("get", "/boards"),
+        );
+      },
+    },
+  );
+
   return (
     <div>
       <h1>Boards list {CONFIG.API_BASE_URL}</h1>
-      <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          createBoardMutation.mutate({
+            body: { name: formData.get("name") as string },
+          });
+        }}
+      >
+        <input type="text" name="name" />
+        <button type="submit" disabled={createBoardMutation.isPending}>
+          Create board
+        </button>
+      </form>
+
+      {boardsQuery.data?.map((board) => (
+        <Card key={board.id}>
+          <CardHeader>
+            <Button asChild variant="link">
+              <Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+                {board.name}
+              </Link>
+            </Button>
+          </CardHeader>
+          <CardFooter>
+            <Button
+              variant="destructive"
+              disabled={deleteBoardMutation.isPending}
+              onClick={() =>
+                deleteBoardMutation.mutate({
+                  params: { path: { boardId: board.id } },
+                })
+              }
+            >
+              Delete
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   );
 }
